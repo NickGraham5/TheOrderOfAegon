054af21cf831842f1733d6977637697a7b125a71
NickGraham5 <43630339+NickGraham5@users.noreply.github.com>
2019-03-07 16:45:39 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@
 //     });
 //   }
 
+function onSignIn(googleUser) 
+{
 var profile = googleUser.getBasicProfile();
    $(".g-signin2").css("display","none");
    $(".data").css("display","block");
