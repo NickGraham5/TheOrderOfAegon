f0c6e7913830dfe56007664d8421e065a20fcf98
NickGraham5 <43630339+NickGraham5@users.noreply.github.com>
2019-03-07 16:11:56 -0600

    updat login

--- js/main.js
+++ js/main.js
@@ -1,21 +1,12 @@
-function onSignIn(googleUser) {
+function onSignIn(googleUser) 
+{
   var profile = googleUser.getBasicProfile();
-   $(".g-signin2").css("display","none");
-   $(".data").css("display","block");
-   $("#pic").attr("src",profile.getImageUrl());
-   $("#email").text(profile.getEmail());
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 }
 
-// <a href="#" onclick="signOut();">Sign out</a>
- function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function ()
-    {
-      console.log('User signed out.');
-      $(".g-signin2").css("display","block");
-      $(".data").css("display","none");
-    });
-  }
 
 
 
@@ -23,12 +14,28 @@ function onSignIn(googleUser) {
 
 
 
-
-// function onSignIn(googleUser) {
-//   var profile = googleUser.getBasicProfile();
-//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-//   console.log('Name: ' + profile.getName());
-//   console.log('Image URL: ' + profile.getImageUrl());
-//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+// var profile = googleUser.getBasicProfile();
+//    $(".g-signin2").css("display","none");
+//    $(".data").css("display","block");
+//    $("#pic").attr("src",profile.getImageUrl());
+//    $("#email").text(profile.getEmail());
 // }
+
+// // <a href="#" onclick="signOut();">Sign out</a>
+// function signOut() 
+// {
+//   var auth2 = gapi.auth2.getAuthInstance();
+//     auth2.signOut().then(function ()
+//     {
+//       console.log('User signed out.');
+//       $(".g-signin2").css("display","block");
+//       $(".data").css("display","none");
+//     });
+// }
+
+
+
+
+
+
 
